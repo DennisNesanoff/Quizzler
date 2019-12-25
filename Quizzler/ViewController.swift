d389a15d99d8a965efe8f679705f0e9e0cc7a892
Dennis Nesanoff <nndennisnn@gmail.com>
2019-12-25 10:42:17 +0300

    final version

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let questions = [Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
+    var questions = [Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
                  Question(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correctAnswer: "100"),
                  Question(q: "What do the letters in the GMT time zone stand for?", a: ["Global Meridian Time", "Greenwich Mean Time", "General Median Time"], correctAnswer: "Greenwich Mean Time"),
                  Question(q: "What is the French word for 'hat'?", a: ["Chapeau", "Écharpe", "Bonnet"], correctAnswer: "Chapeau"),
@@ -29,19 +29,41 @@ class ViewController: UIViewController {
     @IBOutlet var progressBar: UIProgressView!
     
     var currentQuestion = 0
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        questions.shuffle()
         updateUI()
     }
 
-    @IBAction func buttonTapped(_ sender: Any) {
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let answer = questions[currentQuestion].a[sender.tag]
+        
+        if answer == questions[currentQuestion].correctAnswer {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        if currentQuestion + 1 < questions.count {
+            currentQuestion += 1
+        } else {
+            currentQuestion = 0
+            score = 0
+            questions.shuffle()
+        }
+        
+        updateUI()
     }
     
     func updateUI() {
+        scoreLabel.text = "Score: \(score)"
         questioLabel.text = questions[currentQuestion].q
         firstAnswer.setTitle(questions[currentQuestion].a[0], for: .normal)
+        secondAnswer.setTitle(questions[currentQuestion].a[1], for: .normal)
+        thirdAnswer.setTitle(questions[currentQuestion].a[2], for: .normal)
+        progressBar.progress = Float(currentQuestion + 1) / Float(questions.count)
     }
 }
 
