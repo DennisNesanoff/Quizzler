a5cb5652a532372e4c09ac0f82758dfdedf0ff59
Dennis Nesanoff <nndennisnn@gmail.com>
2019-12-25 10:04:37 +0300

    Interface made

---
+++ Quizzler/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Dennis Nesanoff on 25.12.2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+struct Question {
+    var q: String
+    var a: [String]
+    var correctAnswer: String
+}
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let questions = [Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
+                 Question(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correctAnswer: "100"),
+                 Question(q: "What do the letters in the GMT time zone stand for?", a: ["Global Meridian Time", "Greenwich Mean Time", "General Median Time"], correctAnswer: "Greenwich Mean Time"),
+                 Question(q: "What is the French word for 'hat'?", a: ["Chapeau", "Écharpe", "Bonnet"], correctAnswer: "Chapeau"),
+                 Question(q: "In past times, what would a gentleman keep in his fob pocket?", a: ["Notebook", "Handkerchief", "Watch"], correctAnswer: "Watch"),
+                 Question(q: "How would one say goodbye in Spanish?", a: ["Au Revoir", "Adiós", "Salir"], correctAnswer: "Adiós"),
+                 Question(q: "Which of these colours is NOT featured in the logo for Google?", a: ["Green", "Orange", "Blue"], correctAnswer: "Orange"),
+                 Question(q: "What alcoholic drink is made from molasses?", a: ["Rum", "Whisky", "Gin"], correctAnswer: "Rum"),
+                 Question(q: "What type of animal was Harambe?", a: ["Panda", "Gorilla", "Crocodile"], correctAnswer: "Gorilla"),
+                 Question(q: "Where is Tasmania located?", a: ["Indonesia", "Australia", "Scotland"], correctAnswer: "Australia")
+    ]
+    
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var questioLabel: UILabel!
+    @IBOutlet var firstAnswer: UIButton!
+    @IBOutlet var secondAnswer: UIButton!
+    @IBOutlet var thirdAnswer: UIButton!
+    @IBOutlet var progressBar: UIProgressView!
+    
+    var currentQuestion = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    func updateUI() {
+        questioLabel.text = questions[currentQuestion].q
+        firstAnswer.setTitle(questions[currentQuestion].a[0], for: .normal)
+    }
 }
 
